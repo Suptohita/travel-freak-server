c0cf93755cca9509ddb8f7047281fed717e2a69f
Arvind <arbindudebnath59985@gmail.com>
2021-10-29 14:03:23 +0600

    'testing'

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+
+const app = express()
+const port = process.env.PORT || 8000
+
+
+// middleware
+app.use(cors())
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+    res.send('Server Running')
+})
+
+app.listen(port, () => {
+    console.log('Server running at', port)
+})
