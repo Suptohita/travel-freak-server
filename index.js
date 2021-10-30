ff40b18c2fd5612df1962089c20fdd0c808b13a0
Arvind <arbindudebnath59985@gmail.com>
2021-10-30 14:21:04 +0600

    set up database

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const cors = require('cors')
+const {MongoClient} = require('mongodb')
+
 require('dotenv').config()
 
 const app = express()
@@ -11,10 +13,46 @@ app.use(cors())
 app.use(express.json())
 
 
+const uri = "mongodb+srv://travel:LChc1N5sido2XXDX@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+async function run() {
+    try{
+        await client.connect()
+
+        const database = client.db("travel")
+        const destinationsCollection = database.collection("destinations")
+
+
+        
+        // Get API 
+        app.get('/destinations', (req, res) => {
+            console.log('reached destinations...')
+            destinationsCollection.find({}).toArray((req, result) =>{
+                res.send(result)
+            })
+        })
+    }
+
+    finally{
+        // await client.close()
+    }
+}
+
+run().catch(console.dir)
+  
+
+// testing 
 app.get('/', (req, res) => {
     res.send('Server Running....')
 })
 
+app.get('/hello', (req, res) =>{
+    res.send('hello world')
+})
+
+// Listening port 
 app.listen(port, () => {
     console.log('Server running at', port)
 })
