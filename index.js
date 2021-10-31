27e6787ee878c0422f3989f041cd6506484d6374
Arvind <arbindudebnath59985@gmail.com>
2021-10-31 20:57:43 +0600

    set up env

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ app.use(cors())
 app.use(express.json())
 
 
-const uri = "mongodb+srv://travel:LChc1N5sido2XXDX@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.tv1bc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 
 
