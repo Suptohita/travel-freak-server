67436fcf425bc925a253a284f9bc6b888819797d
Arvind <arbindudebnath59985@gmail.com>
2021-10-31 20:02:25 +0600

    implement server for load user data, load data with specific id, post data in database and delete order data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const cors = require('cors')
 const {MongoClient} = require('mongodb')
+const ObjectId = require('mongodb').ObjectId
 
 require('dotenv').config()
 
@@ -23,15 +24,47 @@ async function run() {
 
         const database = client.db("travel")
         const destinationsCollection = database.collection("destinations")
+        const bookingCollection = database.collection('booked-destinations')
 
 
         
-        // Get API 
+        // Get API for home
         app.get('/destinations', async(req, res) => {
             const cursor = destinationsCollection.find({})
             const destinations = await cursor.toArray()
             res.send(destinations)
         })
+
+        // Get Specific destination with id for placeorder
+        app.get('/destinations/:id', async(req, res) => {
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)}
+            const service = await destinationsCollection.findOne(query)
+            res.send(service)
+        })
+
+        // add booking data to database 
+        app.post('/booking', async(req, res) =>{
+            const data = req.body
+            const result = await bookingCollection.insertOne(data)
+            res.send(result)
+        })
+
+        // load data from database for showing user order 
+        app.get('/myorders', async(req, res) => {
+            const cursor = bookingCollection.find({})
+            const userOrders = await cursor.toArray()
+            res.send(userOrders)
+        })
+
+        // delete user order 
+        app.delete('/myorders/deleteorder/:id', async(req, res) => {
+            const id = req.params.id
+            const query = {_id: ObjectId(id)}
+            const result = await bookingCollection.deleteOne(query)
+            res.send(result)
+        })
+    
     }
 
     finally{
